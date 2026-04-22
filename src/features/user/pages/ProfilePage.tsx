33620616ccf44ded55eed29a0415c3b564357047
iamrehman16 <ansarians581@gmail.com>
2026-04-22 21:04:57 +0500

    refactor: optimistic updates for upvotes, added profile page placeholder.

---
+++ src/features/user/pages/ProfilePage.tsx
@@ -0,0 +1,14 @@
+import { Box, Typography } from "@mui/material";
+
+export default function ProfilePage() {
+  return (
+    <Box sx={{ p: 3 }}>
+      <Typography variant="h4" gutterBottom>
+        Profile
+      </Typography>
+      <Typography color="text.secondary">
+        Profile Page Coming soon!
+      </Typography>
+    </Box>
+  );
+}
