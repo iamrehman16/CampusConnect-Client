bb6261b82eadcd94eac3207afb594549e857b2c5
iamrehman16 <ansarians581@gmail.com>
2026-04-24 00:01:24 +0500

    feature: create types and service methods for chat feature

---
+++ src/features/chat/services/chat-service.ts
@@ -0,0 +1,31 @@
+import api from "@/shared/api/axios.instance";
+import type {
+  Conversation,
+  GetMessagesParams,
+  Message,
+  StartConversationDto,
+} from "../types/chat-dto";
+import type { PaginatedResult } from "@/shared/types/api.types";
+
+export class ChatService {
+  async getMyConversations(): Promise<Conversation[]> {
+    const { data } = await api.get<Conversation[]>("conversations");
+    return data;
+  }
+
+  async findOrCreateConversation(
+    dto: StartConversationDto,
+  ): Promise<Conversation> {
+    const { data } = await api.post<Conversation>("conversations", dto);
+    return data;
+  }
+
+  async getMessages(
+    params: GetMessagesParams,
+  ): Promise<PaginatedResult<Message>> {
+    const { data } = await api.get<PaginatedResult<Message>>("messages", {
+      params,
+    });
+    return data;
+  }
+}
---
+++ src/features/chat/types/chat-dto.ts
@@ -0,0 +1,38 @@
+import type { PaginationParams } from "@/shared/types/api.types";
+
+export interface StartConversationDto {
+  participantId: string;
+}
+
+export interface Conversation {
+  participants: string[];
+  lastMessage: string;
+  lastMessageAt: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface createMessageDto {
+  conversationId: string;
+  content: string; //max 2000
+}
+
+export interface DeleteMessageDto {
+  messageId: string;
+  conversationId: string;
+}
+
+export interface GetMessagesParams extends PaginationParams {
+  conversationId: string;
+}
+
+export interface Message {
+  conversationId: string;
+  sender: string;
+  content: string;
+  seen: boolean;
+  seenAt: Date | null;
+  isDeleted: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
